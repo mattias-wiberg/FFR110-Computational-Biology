ebecda943a3f4907369718d4fd154b1510c91d14
Mattias Wiberg <klutten99@gmail.com>
2022-02-18 17:32:53 +0100

    roll added

---
+++ Problem-Set-2/2/2.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def roll(X, step, axis = 1):
+    if axis == 1:
+        match step:
+            case 1:
+                return np.concatenate((np.reshape(X[:,-1], (-1,1)), X[:,:-1]), axis=1)
+            case -1:
+                return np.concatenate((X[:,1:], np.reshape(X[:,0], (-1,1))), axis=1)
+    elif axis == 0:
+        match step:
+            case 1:
+                return np.concatenate((np.reshape(X[-1,:], (1,-1)), X[:-1,:]), axis=0)
+            case -1:
+                return np.concatenate((X[1:,:], np.reshape(X[0,:], (1,-1))), axis=0)
+
+def laplace(X, h=1):
+    # For 2d cartesian coordinates:
+    # delta f = d^2f/dx^2 + d^2f/dy^2
+    # Discrete Laplace operator:
+    # d^2f/dx^2 = (f(x-h) + f(x+h) - 2f(x))/h^2
+    d2fdx2 = (np.roll(X, 1, axis=1) + np.roll(X, -1, axis=1) - 2*X)/h**2
+    d2fdy2 = (np.roll(X, 1, axis=0) + np.roll(X, -1, axis=0) - 2*X)/h**2
+    return d2fdx2 + d2fdy2
+
+L = 128; a = 3; b = 8; Du = 1; u_star = a; v_star = b/a
+Dv = [2.3, 3, 5, 9]; Dv = Dv[3]
+pertubation = 0.1
+tmax = 10000
+dt = 0.01
+u = np.random.uniform(u_star*(1-pertubation), u_star*(1+pertubation), size=(L,L))
+v = np.random.uniform(v_star*(1-pertubation), v_star*(1+pertubation), size=(L,L))
+
+plt.ion()
+for t in np.arange(0,tmax,dt):
+    u += dt*(a-(b+1)*u+u*u*v+Du*laplace(u))
+    v += dt*(b*u-u*u*v+Dv*laplace(v))
+    if t == 1000:
+        plt.imshow(u, vmin=0, vmax=12, cmap="binary")
+        plt.title("t="+str(t)+" Dv="+str(Dv))
+        plt.colorbar()
+        plt.show()
+        input("Done")
+    if int(t*100)%1000==0:
+        progress = round((t/tmax)*100,2)
+        print(f"Simulating: {progress} %")
+    if int(t*100)%10000==0:
+        progress = round((t/tmax)*100,2)
+        print(f"Simulating: {progress} %")
+
+input("Done")
